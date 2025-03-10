abae36da7fb812e427c4a67eee1c46f519ca98a3
Veronika Pozdniakova <800053068@omnia.fi>
2025-03-10 03:11:18 +0200

    Added various languages to learn

--- src/components/CreateNewDeck.jsx
+++ src/components/CreateNewDeck.jsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom"
 import { useState, useEffect, useRef } from 'react'
+import { MAIN_LANGUAGES, OTHER_LANGUAGES } from "../constants/languages"
+import { useErrorNotificationDispatch } from './ErrorNotificationContext'
 
 const CreateNewDeck = () => {
 
   const [isLearnSearchOpen, setIsLearnSearchOpen] = useState(false)
   const [isNatSearchOpen, setIsNatSearchOpen] = useState(false)
+
+  const [learnLanguage, setLearnLanguage] = useState(' 🏳️ Select a language ')
+  const [natLanguage, setNatLanguage] = useState(' 🏳️ Select a language')
+  const [learnFlag, setLearnFlag] = useState('')
+  const [natFlag, setNatFlag] = useState('')
+
+  const errorNotificationDispatch = useErrorNotificationDispatch()
 
   const searchLearnClickHandler = () => {
     setIsLearnSearchOpen(true)
@@ -25,6 +34,18 @@ const CreateNewDeck = () => {
   const dimOverlayHandler = () => {
     searchLearnCancelHandler()
     searchNatCancelHandler()
+  }
+
+  const clickLearnHandler = (language, flag) => {
+    setLearnLanguage(language)
+    setLearnFlag(flag)
+    dimOverlayHandler()
+  }
+
+  const clickNatHandler = (language, flag) => {
+    setNatLanguage(language)
+    setNatFlag(flag)
+    dimOverlayHandler()
   }
 
   useEffect(() => {
@@ -74,6 +95,19 @@ const CreateNewDeck = () => {
     }
   }, [isNatSearchOpen])
 
+  useEffect(() => {
+    if (learnLanguage === natLanguage) {
+      setLearnLanguage(' 🏳️ Select a language ')
+      setNatLanguage(' 🏳️ Select a language')
+      setLearnFlag('')
+      setNatFlag('')
+      errorNotificationDispatch({ type: "SET", payload: 'Cannot learn same languages' })
+      setTimeout(() => {
+        errorNotificationDispatch({ type: "CLEAR" })
+      }, 6000)
+    }
+  }, [learnLanguage, natLanguage])
+
   return (
     <div className="min-h-screen flex flex-col items-center">
       
@@ -89,7 +123,14 @@ const CreateNewDeck = () => {
           <h1 className="font-semibold text-[17px]">Language you want to learn</h1>
           <button onClick={searchLearnClickHandler} className="mt-3 w-[290px] sm:w-full rounded-full border-1 border-green-700 bg-white py-4 pl-7 pr-5 flex flex-row items-center justify-between cursor-default">
 
-            <p>Finnish</p>
+            <p className={`flex flex-row items-center justify-start gap-2 ${learnLanguage === ' 🏳️ Select a language ' ? 'text-gray-500' : 'text-black'}`}>
+              {learnFlag !== '' ? (
+                <img className="w-[20px]" src={`https://flagcdn.com/80x60/${learnFlag}.webp` || null} alt="Learn flag" />
+              ) : (
+                ''
+              )}
+              {learnLanguage}
+            </p>
 
             <svg className="w-[24px]" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ExpandMoreIcon"><path d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z"></path></svg>
 
@@ -100,7 +141,14 @@ const CreateNewDeck = () => {
           <h1 className="font-semibold text-[17px]">Your native Language</h1>
           <button onClick={searchNatClickHandler} className="mt-3 w-[290px] sm:w-full rounded-full border-1 border-green-700 bg-white py-4 pl-7 pr-5 flex flex-row items-center justify-between cursor-default">
 
-            <p>Finnish</p>
+            <p className={`flex flex-row items-center justify-start gap-2 ${natLanguage === ' 🏳️ Select a language' ? 'text-gray-500' : 'text-black'}`}>
+              {natFlag !== '' ? (
+                <img className="w-[20px]" src={`https://flagcdn.com/80x60/${natFlag}.webp` || null} alt="Native flag" />
+              ) : (
+                ''
+              )}
+              {natLanguage}
+            </p>
 
             <svg className="w-[24px]" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ExpandMoreIcon"><path d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z"></path></svg>
 
@@ -170,14 +218,76 @@ const CreateNewDeck = () => {
           </button>
         </div>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col overflow-y-auto w-full p-3">
 
-          <div className="flex flex-col border-b-1 border-[#e2edf5] h-[400px]">
-            
+          <div className="flex flex-col border-b-1 border-[#e2edf5] pb-4">
+            {MAIN_LANGUAGES.map((item, index) => (
+              <button onClick={() => clickLearnHandler(item.language, item.flag)} key={index} className="flex flex-row justify-start items-center hover:bg-gray-100 p-[6px] pl-4 gap-2 cursor-pointer">
+                <img className="w-[20px]" src={`https://flagcdn.com/80x60/${item.flag}.webp`} alt={`${item.language} flag`} />
+                <p>{item.language}</p>
+              </button>
+            ))}
           </div>
 
-          <div className="flex flex-col h-[700px]">
-            
+          <div className="flex flex-col pt-4">
+            {OTHER_LANGUAGES.map((item, index) => (
+              <button onClick={() => clickLearnHandler(item.language, item.flag)} key={index} className="flex flex-row justify-start items-center hover:bg-gray-100 p-[6px] pl-4 gap-2 cursor-pointer">
+                <img className="w-[20px]" src={`https://flagcdn.com/80x60/${item.flag}.webp`} alt={`${item.language} flag`} />
+                <p>{item.language}</p>
+              </button>
+            ))}
+          </div>
+
+        </div>
+
+      </div>
+
+      {/* Native modal menu */}
+
+      <div className={`z-[20] absolute flex flex-col justify-start items-start bg-white w-[600px] h-[500px] my-4 opacity-0 ${isNatSearchOpen ? 'absolute opacity-100' : 'absolute opacity-0 pointer-events-none'}`}>
+
+        <div className="flex flex-row row-span-4 px-1 items-center justify-between h-[50px] w-full border-b-1 border-[#e2edf5]">
+          <svg className="text-[#757575] w-[45px] p-2 rounded-full hover:bg-gray-100" fill="currentColor" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="SearchIcon"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14"></path></svg>
+          <input className="bg-transparent focus:outline-none text-black w-full placeholder-[#707073ff] text-[17px] ml-[5px] cursor-text pb-0.5" type="text" placeholder="Search" />
+          <button
+            type="button"
+            // onClick={clearSearch}
+            aria-label="Clear Search"
+            className="p-2 text-black hover:bg-gray-100 rounded-full"             
+          >
+            <svg className="text-[#acacac] w-[24px]" fill="currentColor" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="BackspaceOutlinedIcon"><path d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m0 16H7.07L2.4 12l4.66-7H22zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path></svg>
+          </button>
+
+          <button
+            type="button"
+            onClick={dimOverlayHandler}
+            aria-label="Clear Search"
+            className="p-2 text-black hover:bg-gray-100 rounded-full"             
+          >
+            <svg className="text-[#acacac]" aria-hidden="true" viewBox="0 0 24 24" role="img" width="23px" height="23px" fill="none" stroke="currentColor" strokeWidth="2">
+              <path d="M18.973 5.027L5.028 18.972m0-13.945l13.944 13.945"></path>
+            </svg>
+          </button>
+        </div>
+
+        <div className="flex flex-col overflow-y-auto w-full p-3">
+
+          <div className="flex flex-col border-b-1 border-[#e2edf5] pb-4">
+            {MAIN_LANGUAGES.map((item, index) => (
+              <button onClick={() => clickNatHandler(item.language, item.flag)} key={index} className="flex flex-row justify-start items-center hover:bg-gray-100 p-[6px] pl-4 gap-2 cursor-pointer">
+                <img className="w-[20px]" src={`https://flagcdn.com/80x60/${item.flag}.webp`} alt={`${item.language} flag`} />
+                <p>{item.language}</p>
+              </button>
+            ))}
+          </div>
+
+          <div className="flex flex-col pt-4">
+            {OTHER_LANGUAGES.map((item, index) => (
+              <button onClick={() => clickNatHandler(item.language, item.flag)} key={index} className="flex flex-row justify-start items-center hover:bg-gray-100 p-[6px] pl-4 gap-2 cursor-pointer">
+                <img className="w-[20px]" src={`https://flagcdn.com/80x60/${item.flag}.webp`} alt={`${item.language} flag`} />
+                <p>{item.language}</p>
+              </button>
+            ))}
           </div>
 
         </div>
---
+++ src/constants/languages.js
@@ -0,0 +1,78 @@
+export const MAIN_LANGUAGES = [
+  {
+    language: 'English',
+    flag: 'gb'
+  },
+  {
+    language: 'Spanish',
+    flag: 'es'
+  },
+  {
+    language: 'French',
+    flag: 'fr'
+  },
+  {
+    language: 'German',
+    flag: 'de'
+  },
+  {
+    language: 'Portuguese',
+    flag: 'pt'
+  },
+  {
+    language: 'Italian',
+    flag: 'it'
+  },
+]
+
+export const OTHER_LANGUAGES = [
+    { language: "Dari", flag: "af" },
+    { language: "Albanian", flag: "al" },
+    { language: "Arabic", flag: "dz" },
+    { language: "Armenian", flag: "am" },
+    { language: "Azerbaijani", flag: "az" },
+    { language: "Arabic", flag: "bh" },
+    { language: "Bengali", flag: "bd" },
+    { language: "Belarusian", flag: "by" },
+    { language: "Dutch", flag: "be" },
+    { language: "Dzongkha", flag: "bt" },
+    { language: "Bosnian", flag: "ba" },
+    { language: "Malay", flag: "bn" },
+    { language: "Bulgarian", flag: "bg" },
+    { language: "Kirundi", flag: "bi" },
+    { language: "Khmer", flag: "kh" },
+    { language: "Mandarin Chinese", flag: "cn" },
+    { language: "Comorian", flag: "km" },
+    { language: "Croatian", flag: "hr" },
+    { language: "Greek", flag: "cy" },
+    { language: "Czech", flag: "cz" },
+    { language: "Danish", flag: "dk" },
+    { language: "Estonian", flag: "ee" },
+    { language: "Amharic", flag: "et" },
+    { language: "Finnish", flag: "fi" },
+    { language: "Greek", flag: "gr" },
+    { language: "Haitian Creole", flag: "ht" },
+    { language: "Hindi", flag: "in" },
+    { language: "Indonesian", flag: "id" },
+    { language: "Persian", flag: "ir" },
+    { language: "Hebrew", flag: "il" },
+    { language: "Japanese", flag: "jp" },
+    { language: "Swahili", flag: "ke" },
+    { language: "Korean", flag: "kr" },
+    { language: "Malay", flag: "my" },
+    { language: "Dutch", flag: "nl" },
+    { language: "Norwegian", flag: "no" },
+    { language: "Urdu", flag: "pk" },
+    { language: "Filipino", flag: "ph" },
+    { language: "Polish", flag: "pl" },
+    { language: "Romanian", flag: "ro" },
+    { language: "Russian", flag: "ru" },
+    { language: "Arabic", flag: "sa" },
+    { language: "Swedish", flag: "se" },
+    { language: "Thai", flag: "th" },
+    { language: "Turkish", flag: "tr" },
+    { language: "Ukrainian", flag: "ua" },
+    { language: "Arabic", flag: "ae" },
+    { language: "Vietnamese", flag: "vn" },
+    { language: "Zulu", flag: "za" }
+]
