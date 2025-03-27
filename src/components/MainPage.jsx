11750ce43fe9322b940c35f7789adf947a411135
Ruslan <rus.onypenko@gmail.com>
2025-03-27 22:01:25 +0200

    Must do updating chosenVoice

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom"
-import axios from 'axios'
 import { useState, useEffect } from "react";
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import decksService from '../services/decks'
@@ -17,15 +16,15 @@ const MainPage = ({ decks, onDeckChange }) => {
 
   const [voices, setVoices] = useState([])
 
-  const [voicesWindowOpen, setVoicesWindowOpen] = useState(false)
+  const [currentVoices, setCurrentVoices] = useState([])
 
-  const [language, setLanguage] = useState('en')
+  const [chosenVoice, setChosenVoice] = useState('')
+
+  const [voicesWindowOpen, setVoicesWindowOpen] = useState(false)
 
   const [selectedValue, setSelectedValue] = useState('All')
 
   const [searchValue, setSearchValue] = useState('')
-
-  console.log(language)
 
   useEffect(() => {
     const loadVoices = () => {
@@ -37,51 +36,30 @@ const MainPage = ({ decks, onDeckChange }) => {
     loadVoices()
   }, [])
 
-  const wordSpeakingHandler = (word) => {
-    // detectLanguage(word)
-    speak(word)
-  }
+  useEffect(() => {
+    const filteredVoices = voices.filter((v) => v.name.includes(currentDeck.learnLang))
 
-  // const detectLanguage = async (word) => {
-  //   try {
-  //       const response = await axios.post(
-  //         "https://ws.detectlanguage.com/0.2/detect",
-  //         { q: word },
-  //         { headers: { Authorization: "Bearer 1c156fa3a24a284898fa4808b18da1ac" } }
-  //       )
-  //       const detectedLang = response.data.data.detections[0].language
-  //       if (language !== detectedLang) {
-  //         setLanguage(detectedLang)
-  //       }
-  //   } catch (error) {
-  //     console.error("Error detecting language:", error);
-  //   }
-  // }
+    setCurrentVoices(filteredVoices)
+
+    // ***************
+    setChosenVoice(filteredVoices[0])
+  }, [voices, currentDeck.learnLang])
 
   const speak = (word) => {
 
-    // if (!language) {
-    //   detectLanguage(word).then(() => speak(word));
-    //   return;
-    // }
-
-    // detectLanguage(word)
-
     if (!voices.length) return
 
-    const filteredVoices = voices.filter((v) => v.lang.startsWith(language))
-
-    if (!filteredVoices.length) {
-      alert('No voice found')
-      return
-    }
-
-    const selectedVoice = filteredVoices[5];
+    const selectedVoice = chosenVoice;
 
     const utterance = new SpeechSynthesisUtterance(word)
     utterance.voice = selectedVoice
 
     speechSynthesis.speak(utterance)
+  }
+
+  const handleVoiceChange = (voice) => {
+    // *****************
+    setChosenVoice(voice)
   }
 
   const dropdownOptions = [
@@ -416,12 +394,30 @@ const MainPage = ({ decks, onDeckChange }) => {
                   className="p-1.5 pl-2 border-l-1 hover:bg-gray-100 border-l-[#eeeeee] relative">
                   <svg className="w-[25px] text-[#8a8a8a]" fill="currentColor" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="RecordVoiceOverIcon"><circle cx="9" cy="9" r="4"></circle><path d="M9 15c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4m7.76-9.64-1.68 1.69c.84 1.18.84 2.71 0 3.89l1.68 1.69c2.02-2.02 2.02-5.07 0-7.27M20.07 2l-1.63 1.63c2.77 3.02 2.77 7.56 0 10.74L20.07 16c3.9-3.89 3.91-9.95 0-14"></path></svg>
                 
-                  <div className={`bg-white w-[200px] py-2 h-[200px] flex flex-col rounded-lg shadow-[0px_-4px_4px_rgba(0,0,0,0.02),0px_4px_4px_rgba(0,0,0,0.02),-4px_0px_4px_rgba(0,0,0,0.02),4px_0px_4px_rgba(0,0,0,0.02)] bottom-0 right-0 transition-all duration-200 ${ voicesWindowOpen ? 'absolute opacity-100' : 'absolute opacity-0 pointer-events-none'}`} onMouseEnter={() => handleVoicesEnter} onMouseLeave={handleVoicesLeave}>
-                      {/* {voices
+                  <div className={`bg-white w-[200px] px-2 py-2 h-[165px] flex flex-col rounded-lg shadow-[0px_-4px_4px_rgba(0,0,0,0.02),0px_4px_4px_rgba(0,0,0,0.02),-4px_0px_4px_rgba(0,0,0,0.02),4px_0px_4px_rgba(0,0,0,0.02)] bottom-0 right-0 transition-all duration-200 ${ voicesWindowOpen ? 'absolute opacity-100' : 'absolute opacity-0 pointer-events-none'}`} onMouseEnter={() => handleVoicesEnter} onMouseLeave={handleVoicesLeave}>
+                      {currentVoices
                         .slice(0, 6)
-                        .map((voice, index) => (
-                        <p></p>
-                      ))} */}
+                        .map((voice, index) => {
+                          const voiceName = voice.name.split(" ")[1] || voice.name
+                          return (
+                            <label key={index} className="flex items-center gap-2 cursor-pointer">
+                              <input
+                                type="radio"
+                                name="voice"
+                                value={voiceName}
+                                checked={chosenVoice === voice}
+                                onChange={() => handleVoiceChange(voice)}
+                                className="hidden"
+                              />
+                              <div
+                                className={`w-4 h-4 border-2 rounded-full ${
+                                  chosenVoice === voice ? "bg-green-500 border-green-500" : "border-gray-500"
+                                }`}
+                              ></div>
+                              <span>{voiceName}</span>
+                            </label>
+                          )
+                        })}
                   </div>
                 
                 </div>
@@ -495,14 +491,14 @@ const MainPage = ({ decks, onDeckChange }) => {
                       <img className="w-[50px] h-[50px]" src={`http://localhost:3003/api/images/files/${card.img}`} alt="Card Images"/>
                       <div className="flex flex-col ml-0.5 items-start">
                         <div className="flex flex-row gap-1">
-                          <button onClick={() => wordSpeakingHandler(card.word)} className="hover:bg-gray-100 p-1 rounded-full cursor-pointer">
+                          <button onClick={() => speak(card.word)} className="hover:bg-gray-100 p-1 rounded-full cursor-pointer">
                             <svg className="w-[15px] text-green-600" aria-hidden="true" focusable="false" data-prefix="far" data-icon="volume" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M191.9 201.9L304 102.3V409.7L191.9 310.1c-4.4-3.9-10.1-6.1-15.9-6.1H88c-4.4 0-8-3.6-8-8V216c0-4.4 3.6-8 8-8h88c5.9 0 11.6-2.2 15.9-6.1zM322.2 32c-7.3 0-14.3 2.7-19.8 7.5L166.9 160H88c-30.9 0-56 25.1-56 56v80c0 30.9 25.1 56 56 56h78.9L302.4 472.5c5.5 4.8 12.5 7.5 19.8 7.5c16.5 0 29.8-13.3 29.8-29.8V61.8C352 45.3 338.7 32 322.2 32zm182.9 75c-10.3-8.4-25.4-6.8-33.8 3.5s-6.8 25.4 3.5 33.8C507.3 170.7 528 210.9 528 256s-20.7 85.3-53.2 111.8c-10.3 8.4-11.8 23.5-3.5 33.8s23.5 11.8 33.8 3.5c43.2-35.2 70.9-88.9 70.9-149s-27.7-113.8-70.9-149zm-60.5 74.5c-10.3-8.4-25.4-6.8-33.8 3.5s-6.8 25.4 3.5 33.8C425.1 227.6 432 241 432 256s-6.9 28.4-17.7 37.3c-10.3 8.4-11.8 23.5-3.5 33.8s23.5 11.8 33.8 3.5C466.1 312.9 480 286.1 480 256s-13.9-56.9-35.4-74.5z"></path></svg>
                           </button>
                           <p className="font-semibold text-[14px]">{card.word}</p>
                         </div>
                         <p className="text-[13.5px] ml-[27px]">{card.translation}</p>
                         <div className="flex flex-row gap-1">
-                          <button onClick={() => wordSpeakingHandler(card.usage)} className="hover:bg-gray-100 p-1 rounded-full cursor-pointer">
+                          <button onClick={() => speak(card.usage)} className="hover:bg-gray-100 p-1 rounded-full cursor-pointer">
                             <svg className="w-[15px] text-green-600" aria-hidden="true" focusable="false" data-prefix="far" data-icon="volume" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M191.9 201.9L304 102.3V409.7L191.9 310.1c-4.4-3.9-10.1-6.1-15.9-6.1H88c-4.4 0-8-3.6-8-8V216c0-4.4 3.6-8 8-8h88c5.9 0 11.6-2.2 15.9-6.1zM322.2 32c-7.3 0-14.3 2.7-19.8 7.5L166.9 160H88c-30.9 0-56 25.1-56 56v80c0 30.9 25.1 56 56 56h78.9L302.4 472.5c5.5 4.8 12.5 7.5 19.8 7.5c16.5 0 29.8-13.3 29.8-29.8V61.8C352 45.3 338.7 32 322.2 32zm182.9 75c-10.3-8.4-25.4-6.8-33.8 3.5s-6.8 25.4 3.5 33.8C507.3 170.7 528 210.9 528 256s-20.7 85.3-53.2 111.8c-10.3 8.4-11.8 23.5-3.5 33.8s23.5 11.8 33.8 3.5c43.2-35.2 70.9-88.9 70.9-149s-27.7-113.8-70.9-149zm-60.5 74.5c-10.3-8.4-25.4-6.8-33.8 3.5s-6.8 25.4 3.5 33.8C425.1 227.6 432 241 432 256s-6.9 28.4-17.7 37.3c-10.3 8.4-11.8 23.5-3.5 33.8s23.5 11.8 33.8 3.5C466.1 312.9 480 286.1 480 256s-13.9-56.9-35.4-74.5z"></path></svg>
                           </button>
                           <p className="text-[13.5px] text-[#afafaf]">{card.usage}</p>
