8e863fa0b1e074c6844c8af062c0188c207f487f
Veronika Pozdniakova <800053068@omnia.fi>
2025-04-09 03:13:12 +0300

    Implementation of 'life deck'

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -4,6 +4,7 @@ import { useErrorNotificationDispatch } from './ErrorNotificationContext'
 import { useNotificationDispatch } from "./NoificationContext"
 import { useLocation } from 'react-router-dom'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { motion, AnimatePresence } from "framer-motion"
 
 const Learn = () => {
 
@@ -12,7 +13,35 @@ const Learn = () => {
   const location = useLocation()
   const currentDeck = location.state?.currentDeck
 
-  console.log(currentDeck)
+  // console.log(currentDeck.cards)
+
+  const [toLearnCards, setToLearnCards] = useState([])
+
+  useEffect(() => {
+    setToLearnCards(currentDeck?.cards?.filter(card => card.toLearn === true))
+  }, [])
+
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [isFlipped, setIsFlipped] = useState(false)
+  const [wiggle, setWiggle] = useState(false)
+
+  const handleFlip = () => {
+    if (!isFlipped) setIsFlipped(true)
+  }
+
+  const handleSwipe = () => {
+    setIsFlipped(false)
+    setCurrentIndex((prev) => prev + 1)
+  }
+
+  const handleBackClick = () => {
+    setWiggle(true)
+    setTimeout(() => setWiggle(false), 500)
+  }
+
+  const currentCard = toLearnCards[currentIndex]
+
+  console.log(toLearnCards)
 
   return (
     <div className="min-h-screen flex flex-col items-center w-full">
@@ -27,10 +56,58 @@ const Learn = () => {
         </div>
       </div>
 
-      <div className="flex-1 flex pb-[55px] flex-col items-center justify-center w-full bg-[#f3fff2]">
-        <div className="flex flex-col items-center justify-center w-[280px] sm:w-[480px] h-[360px] bg-white rounded-xl shadow-lg border-1 border-[#e8e8e8]">
-          Card
-        </div>
+      <div className="relative overflow-hidden flex-1 flex pb-[55px] flex-col items-center justify-center w-full bg-[#f3fff2]">
+        <AnimatePresence>
+          {currentCard && (
+            <motion.div
+              key={currentCard.id}
+              className="absolute flex flex-col items-center justify-center w-[280px] sm:w-[480px] h-[360px] rounded-xl shadow-lg border border-[#e8e8e8] bg-white"
+              onClick={handleFlip}
+              initial={{ opacity: 0 }}
+              exit={{ opacity: 0, scale: 0.8 }}
+              drag={isFlipped ? "x" : false}
+              dragConstraints={{ left: 0, right: 0 }}
+              onDragEnd={(event, info) => {
+                if (info.offset.x > 100 || info.offset.x < -100) {
+                  handleSwipe()
+                }
+              }}
+            whileTap={{ scale: 0.98 }}
+            animate={wiggle ? { rotate: [0, -10, 10, -8, 8, 0], opacity: 1 } : { opacity: 1, y: 0, x:0, rotate: 0 }}
+            transition={wiggle ? { duration: 0.5 } : {duration: 0.3}}
+            >
+              <div className={`absolute w-full h-full rounded-xl backface-hidden flex flex-col items-center justify-center transition-transform duration-500 ${isFlipped ? 'rotate-y-180' : ''}`}>
+                  <img className="w-[120px] select-none pointer-events-none user-select-none" src={`http://localhost:3003/api/images/files/${currentCard.img}`} alt={currentCard.word} />
+                  <p className="text-[22px] mt-[12px] select-none">{currentCard.translation}</p>
+              </div>
+              <div onClick={handleBackClick} className={`absolute w-full h-full bg-white rounded-xl flex flex-col items-center justify-center text-xl font-semibold transition-transform duration-500 transform ${isFlipped ? '' : 'rotate-y-180'} backface-hidden`}>
+                  <img className="w-[120px] select-none pointer-events-none user-select-none" src={`http://localhost:3003/api/images/files/${currentCard.img}`} alt={currentCard.word} />
+                  <p className="text-[16px] mt-[12px] text-[#707070] select-none">{currentCard.translation}</p>
+                  <div className="flex flex-row items-center justify-center gap-1 mt-3 border-b-1 border-[#e2edf5] pb-4">
+                    <button 
+                      onClick={(e) => {
+                      e.stopPropagation()
+                      // handleVoicesEnter()
+                      }}
+                      className="hover:bg-gray-100 p-1 rounded-full cursor-pointer"
+                      >
+                      <svg className="w-[20px] h-[20px] text-green-600" aria-hidden="true" focusable="false" data-prefix="far" data-icon="volume" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M191.9 201.9L304 102.3V409.7L191.9 310.1c-4.4-3.9-10.1-6.1-15.9-6.1H88c-4.4 0-8-3.6-8-8V216c0-4.4 3.6-8 8-8h88c5.9 0 11.6-2.2 15.9-6.1zM322.2 32c-7.3 0-14.3 2.7-19.8 7.5L166.9 160H88c-30.9 0-56 25.1-56 56v80c0 30.9 25.1 56 56 56h78.9L302.4 472.5c5.5 4.8 12.5 7.5 19.8 7.5c16.5 0 29.8-13.3 29.8-29.8V61.8C352 45.3 338.7 32 322.2 32zm182.9 75c-10.3-8.4-25.4-6.8-33.8 3.5s-6.8 25.4 3.5 33.8C507.3 170.7 528 210.9 528 256s-20.7 85.3-53.2 111.8c-10.3 8.4-11.8 23.5-3.5 33.8s23.5 11.8 33.8 3.5c43.2-35.2 70.9-88.9 70.9-149s-27.7-113.8-70.9-149zm-60.5 74.5c-10.3-8.4-25.4-6.8-33.8 3.5s-6.8 25.4 3.5 33.8C425.1 227.6 432 241 432 256s-6.9 28.4-17.7 37.3c-10.3 8.4-11.8 23.5-3.5 33.8s23.5 11.8 33.8 3.5C466.1 312.9 480 286.1 480 256s-13.9-56.9-35.4-74.5z"></path></svg>
+                    </button>
+                    <p className="text-[20px] pb-1 select-none">{currentCard.word}</p>
+                  </div>
+                  <button
+                      onClick={(e) => {
+                      e.stopPropagation()
+                      // handleEdit()
+                      }}
+                      className="hover:bg-gray-100 p-1.5 mt-4 rounded-full cursor-pointer"
+                      >
+                    <svg className="w-[18px] h-[18px] text-green-600" aria-hidden="true" focusable="false" data-prefix="far" data-icon="pen-to-square" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"></path></svg>
+                  </button>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
 
       <div className="fixed bottom-0 z-20 w-full flex-none flex flex-row items-center justify-center gap-45 sm:gap-100 h-[55px]">
