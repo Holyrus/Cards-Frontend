4d176bd0ddea4eb6143f4e711434870f8ce136aa
Ruslan <rus.onypenko@gmail.com>
2025-04-25 19:45:20 +0300

    Continue black mode on main menu

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -571,7 +571,7 @@ const MainPage = ({ decks, onDeckChange }) => {
 
         </div>
         <ThreeDModel />
-        { currentDeck?.cards?.length !== 0 && currentDeck && <Link to='/main/learn' state={{ currentDeck }} ><button className={`rounded-full font-semibold py-2 px-5 w-[290px] sm:w-[400px] md:w-[450px] md:py-3 shadow-md hover:shadow-lg transition-all duration-300 select-none
+        { currentDeck?.cards?.length !== 0 && currentDeck && <Link to='/main/learn' state={{ currentDeck }} ><button className={`rounded-full font-semibold py-2 px-5 w-[290px] sm:w-[400px] md:w-[450px] md:py-3 shadow-md hover:shadow-lg transition-all duration-300 select-none cursor-pointer
           ${theme === 'Black' ? 'text-black bg-green-700 hover:bg-green-600' : 'text-white border-1 border-green-700 bg-green-700 hover:bg-green-100 hover:text-green-700'}`}>START</button></Link> }
           <div className="mt-5 flex flex-row justify-center items-center gap-3">
 
@@ -599,7 +599,7 @@ const MainPage = ({ decks, onDeckChange }) => {
                   className={`p-1.5 pl-2 relative ${theme === 'Black' ? 'border-l-1 hover:bg-[#1e1e1e] border-l-[#646464]' : 'border-l-1 hover:bg-gray-100 border-l-[#eeeeee]'}`}>
                   <svg className="w-[25px] text-[#8a8a8a]" fill="currentColor" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="RecordVoiceOverIcon"><circle cx="9" cy="9" r="4"></circle><path d="M9 15c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4m7.76-9.64-1.68 1.69c.84 1.18.84 2.71 0 3.89l1.68 1.69c2.02-2.02 2.02-5.07 0-7.27M20.07 2l-1.63 1.63c2.77 3.02 2.77 7.56 0 10.74L20.07 16c3.9-3.89 3.91-9.95 0-14"></path></svg>
                 
-                  <div className={`${theme === 'Black' ? 'C text-white shadow-[0px_-4px_4px_rgba(0,0,0,0.02),0px_4px_4px_rgba(0,0,0,0.02),-4px_0px_4px_rgba(0,0,0,0.02),4px_0px_4px_rgba(0,0,0,0.02)] shadow-[#272725]' : 'bg-white'} w-auto px-2 py-2 h-[180px] overflow-y-scroll overflow-x-hidden flex flex-col rounded-lg bottom-0 right-[-100px] sm:right-0 transition-all duration-200 ${ voicesWindowOpen ? 'absolute opacity-100' : 'absolute opacity-0 pointer-events-none'}`} onMouseEnter={() => handleVoicesEnter} onMouseLeave={handleVoicesLeave}>
+                  <div className={`${theme === 'Black' ? 'bg-[#0d0d0d] text-white shadow-[0px_-4px_4px_rgba(0,0,0,0.02),0px_4px_4px_rgba(0,0,0,0.02),-4px_0px_4px_rgba(0,0,0,0.02),4px_0px_4px_rgba(0,0,0,0.02)] shadow-[#272725]' : 'bg-white'} w-auto px-2 py-2 h-[180px] overflow-y-scroll overflow-x-hidden flex flex-col rounded-lg bottom-0 right-[-100px] sm:right-0 transition-all duration-200 ${ voicesWindowOpen ? 'absolute opacity-100' : 'absolute opacity-0 pointer-events-none'}`} onMouseEnter={() => handleVoicesEnter} onMouseLeave={handleVoicesLeave}>
                       {currentVoices
                         .map((voice, index) => {
                           const voiceName = voice.name.split(" ")[1] || voice.name
@@ -690,7 +690,8 @@ const MainPage = ({ decks, onDeckChange }) => {
               }
 
           { currentDeck?.cards?.length !== 0 && currentDeck && isCardsOpen && 
-            <div className="bg-white w-[270px] sm:w-[500px] md:w-[700px] lg:[800px] flex flex-col">
+            <div className={`w-[270px] sm:w-[500px] md:w-[700px] lg:[800px] flex flex-col
+              ${theme === 'Black' ? 'bg-[#0d0d0d]' : 'bg-white'}`}>
               { currentDeck.cards
                 .filter(card => {
                   const textMatch =
@@ -743,7 +744,8 @@ const MainPage = ({ decks, onDeckChange }) => {
                 }
 
                 return (
-                  <div className="min-h-[90px] py-2 w-full border-b-1 border-[#e2edf5] flex flex-row items-center justify-between px-4" key={card.id}>
+                  <div className={`min-h-[90px] py-2 w-full flex flex-row items-center justify-between px-4
+                    ${theme === 'Black' ? 'text-white border-b-1 border-[#182026]' : 'text-black border-b-1 border-[#e2edf5]'}`} key={card.id}>
 
                     <div className="relative flex flex-row items-center">
                       { card.toLearn ? (
@@ -783,16 +785,20 @@ const MainPage = ({ decks, onDeckChange }) => {
                     <button onMouseEnter={() => handleSettingsEnter(card.id)} onMouseLeave={handleSettingsLeave} className="py-2 rounded-full relative">
                       <svg className="w-[25px] text-[#757575]" fill="currentColor" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="MoreVertIcon"><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2m0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2m0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2"></path></svg>
 
-                      <div className={`bg-white w-[118px] py-2 h-[160px] flex flex-col rounded-lg shadow-[0px_-4px_4px_rgba(0,0,0,0.02),0px_4px_4px_rgba(0,0,0,0.02),-4px_0px_4px_rgba(0,0,0,0.02),4px_0px_4px_rgba(0,0,0,0.02)] bottom-0 right-0 lg:left-0 transition-all duration-200 ${activeSettingsId === card.id ? 'absolute opacity-100' : 'absolute opacity-0 pointer-events-none'}`} onMouseEnter={() => handleSettingsEnter(card.id)} onMouseLeave={handleSettingsLeave}>
-                        <Link to={`/card/${card.id}`} className="flex flex-row items-center justify-start w-full h-[36px] hover:bg-[#f5f5f5] p-2 pl-4 gap-3">
+                      <div className={`w-[118px] py-2 z-15 h-[160px] flex flex-col rounded-lg shadow-[0px_-4px_4px_rgba(0,0,0,0.02),0px_4px_4px_rgba(0,0,0,0.02),-4px_0px_4px_rgba(0,0,0,0.02),4px_0px_4px_rgba(0,0,0,0.02)] bottom-0 right-0 lg:left-0 transition-all duration-200 ${activeSettingsId === card.id ? 'absolute opacity-100' : 'absolute opacity-0 pointer-events-none'}
+                        ${theme === 'Black' ? 'text-white bg-[#0d0d0d] shadow-[#272725]' : 'text-black bg-white'}`} onMouseEnter={() => handleSettingsEnter(card.id)} onMouseLeave={handleSettingsLeave}>
+                        <Link to={`/card/${card.id}`} className={`flex flex-row items-center justify-start w-full h-[36px] p-2 pl-4 gap-3
+                          ${theme === 'Black' ? 'hover:bg-[#1e1e1e]' : 'hover:bg-[#f5f5f5]'}`}>
                           <svg className="w-[18px]" aria-hidden="true" focusable="false" data-prefix="far" data-icon="pen-to-square" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"></path></svg>
                           <p>Edit</p>
                         </Link>
-                        <div onClick={() => cardResetHandler(card)} className="flex flex-row items-center justify-start w-full h-[36px] hover:bg-[#f5f5f5] p-2 pl-4 gap-3">
+                        <div onClick={() => cardResetHandler(card)} className={`flex flex-row items-center justify-start w-full h-[36px] p-2 pl-4 gap-3
+                          ${theme === 'Black' ? 'hover:bg-[#1e1e1e]' : 'hover:bg-[#f5f5f5]'}`}>
                           <svg className="w-[18px]" aria-hidden="true" focusable="false" data-prefix="far" data-icon="arrows-rotate" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M94 187.1C120.8 124.1 183.3 80 256 80c39.7 0 77.8 15.8 105.9 43.9L414.1 176H360c-13.3 0-24 10.7-24 24s10.7 24 24 24H472c13.3 0 24-10.7 24-24V88c0-13.3-10.7-24-24-24s-24 10.7-24 24v54.1L395.9 89.9C358.8 52.8 308.5 32 256 32C163.4 32 83.9 88.2 49.8 168.3c-5.2 12.2 .5 26.3 12.7 31.5s26.3-.5 31.5-12.7zm368 157c5.2-12.2-.4-26.3-12.6-31.5s-26.3 .4-31.5 12.6C391 388.1 328.6 432 256 432c-39.7 0-77.8-15.8-105.9-43.9L97.9 336H152c13.3 0 24-10.7 24-24s-10.7-24-24-24H40c-13.3 0-24 10.7-24 24V424c0 13.3 10.7 24 24 24s24-10.7 24-24V369.9l52.1 52.1C153.2 459.2 203.5 480 256 480c92.5 0 171.8-56 206-135.9z"></path></svg>
                           <p>Reset</p>
                         </div>
-                        <div onClick={() => cardDeleteHandler(card.id)} className="flex flex-row items-center justify-start w-full h-[36px] hover:bg-[#f5f5f5] p-2 pl-4 gap-3">
+                        <div onClick={() => cardDeleteHandler(card.id)} className={`flex flex-row items-center justify-start w-full h-[36px] p-2 pl-4 gap-3
+                          ${theme === 'Black' ? 'hover:bg-[#1e1e1e]' : 'hover:bg-[#f5f5f5]'}`}>
                           <svg className="w-[18px] text-[#dd4444]" fill="currentColor" aria-hidden="true" focusable="false" data-prefix="far" data-icon="trash-xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80h13.7H416h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H412.4L388.4 452.7c-2.5 33.4-30.3 59.3-63.8 59.3H123.4c-33.5 0-61.3-25.9-63.8-59.3L35.6 128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80.1 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM83.7 128l23.8 321.2c.6 8.4 7.6 14.8 16 14.8H324.6c8.4 0 15.3-6.5 16-14.8L364.3 128H83.7zm62.2 81.9c7.8-7.8 20.5-7.8 28.3 0L224 259.7l49.9-49.9c7.8-7.8 20.5-7.8 28.3 0s7.8 20.5 0 28.3L252.3 288l49.9 49.9c7.8 7.8 7.8 20.5 0 28.3s-20.5 7.8-28.3 0L224 316.3l-49.9 49.9c-7.8 7.8-20.5 7.8-28.3 0s-7.8-20.5 0-28.3L195.7 288l-49.9-49.9c-7.8-7.8-7.8-20.5 0-28.3z"></path></svg>
                           <p className="text-[#dd4444]">Delete</p>
                         </div>
@@ -809,11 +815,13 @@ const MainPage = ({ decks, onDeckChange }) => {
 
       </div>
 
-      <div className="fixed bottom-0 z-20 w-full flex-none flex flex-row items-center justify-center border-t-1 border-[#e1edf5] bg-white h-[55px]">
-        <Link to="/main" className="flex flex-col items-center justify-start px-[60px] hover:cursor-pointer active:bg-gray-300 transition-all duration-75" draggable="false">
+      <div className={`fixed bottom-0 z-20 w-full flex-none flex flex-row items-center justify-center h-[55px]
+        ${theme === 'Black' ? 'border-t-1 border-[#182026] bg-[#0d0d0d]' : 'border-t-1 border-[#e1edf5] bg-white'}`}>
+        <Link to="/main" className={`flex flex-col items-center justify-start px-[60px] hover:cursor-pointer transition-all duration-75
+          ${theme === 'Black' ? 'active:bg-[#1e1e1e]' : 'active:bg-gray-300'}`} draggable="false">
           <svg width="35" height="35" viewBox="0 0 200 250" xmlns="http://www.w3.org/2000/svg">
             <g transform="rotate(10, 115, 130)">
-              <rect x="80" y="80" width="90" height="140" rx="10" ry="10" fill="black"/>
+              <rect x="80" y="80" width="90" height="140" rx="10" ry="10" fill={`${theme === 'Black' ? '#c4c4c4' : 'black'}`}/>
             </g>
             <g transform="rotate(-10, 85, 100)">
               <rect x="30" y="30" width="90" height="140" rx="10" ry="10" fill="green"/>
